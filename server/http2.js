650c6292c0c270ad94d48151a857e22058d4ed4e
hhhhhhjs <1512403978@qq.com>
2023-12-11 18:17:10 +0800

    :feet:http服务器和数据库联通

--- server/http2.js
+++ server/http2.js
@@ -1,20 +1,26 @@
 import http from 'node:http'
+import { getAllfiles } from '../database/file.js'
 
 const router = [
     { path: '/', handle: async (ctx) => { ctx.res.end('根路径') } },
     { path: '/login', handle: async (ctx) => {
         ctx.res.end('登录')
         console.log(ctx.query)
-    }}
+    }},
+    {
+        path:'/list',handle: async(ctx) => {
+            const result = {
+                statusCode:'200',
+                info:'ok',
+                data:getAllfiles()
+            }
+            ctx.res.end(JSON.stringify(result))
+        }
+    }
 ]
 
 const server = http.createServer(async (req, res) => {
-    const { url, param } = uriparser(req.url) // 修改这里的res.url为req.url
-    const context = {
-        req: req,
-        res: res,
-        query: param
-    }
+    
 
     function uriparser(originurl) {
         const result = {}
@@ -22,7 +28,7 @@ const server = http.createServer(async (req, res) => {
         const url = params[0]
         if (params.length === 2) {
             const pArr = params[1].split('&')
-            pArr.forEach(pair => { // 修改此处的foreach为forEach
+            pArr.forEach(pair => { 
                 const pairArr = pair.split('=')
                 result[pairArr[0]] = pairArr[1]
             })
@@ -37,6 +43,14 @@ const server = http.createServer(async (req, res) => {
             }
         }
     }
+    const { url, param } = uriparser(req.url)//解构赋值，将对象中的key赋值，此时是一个变量，而不是key
+    
+    const context = {
+        req: req,
+        res: res,
+        query: param
+    }
+    
 
     const routerInstance = router.find(r => r.path === url)
     if (routerInstance) {
